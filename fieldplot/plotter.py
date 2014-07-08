88d701c786855c0a2c9cd32f1f957f7f9d14ed46
Aritra Biswas <abiswas@glycine.localdomain>
2014-07-08 10:52:07 -0700

    Fully fixed flag support

--- fieldplot/plotter.py
+++ fieldplot/plotter.py
@@ -49,34 +49,35 @@ if __name__ == '__main__':
 
     for arg in sys.argv:
         if arg == "-x":
-            x_show = True; no_flags = False
+            x_show = True
             horiz += 1
             first_arg +=1
         elif arg == "-y":
-            y_show = True; no_flags = False
+            y_show = True
             horiz += 1
             first_arg += 1
         elif arg == "-z":
-            z_show = True; no_flags = False
+            z_show = True
             horiz += 1
             first_arg += 1
         elif arg == "-Bx":
-            Bx_show = True; no_flags = False
+            Bx_show = True
             vert += 1
             first_arg += 1
         elif arg == "-By":
-            By_show = True; no_flags = False
+            By_show = True
             vert += 1
             first_arg += 1
         elif arg == "-Bz":
-            Bz_show = True; no_flags = False
+            Bz_show = True
             vert += 1
             first_arg += 1
 
-    if no_flags:
+    if x_show == False and y_show == False and z_show == False:
         x_show = True
         y_show = True
         z_show = True
+    if Bx_show == False and By_show == False and Bz_show == False:
         Bx_show = True
         By_show = True
         Bz_show = True
@@ -93,30 +94,30 @@ if __name__ == '__main__':
     i = 1
     
     if Bx_show == True and x_show == True:
-        Bx_v_x = fig.add_subplot(horiz, vert, i); i += 1
+        Bx_v_x = fig.add_subplot(vert, horiz, i, xlabel = "x", ylabel = "Bx"); i += 1
     if Bx_show == True and y_show == True:
-        Bx_v_y = fig.add_subplot(horiz, vert, i); i += 1
+        Bx_v_y = fig.add_subplot(vert, horiz, i, xlabel = "y", ylabel = "Bx"); i += 1
     if Bx_show == True and z_show == True:
-        Bx_v_z = fig.add_subplot(horiz, vert, i); i += 1
+        Bx_v_z = fig.add_subplot(vert, horiz, i, xlabel = "z", ylabel = "Bx"); i += 1
     if By_show == True and x_show == True:
-        By_v_x = fig.add_subplot(horiz, vert, i); i += 1
+        By_v_x = fig.add_subplot(vert, horiz, i, xlabel = "x", ylabel = "By"); i += 1
     if By_show == True and y_show == True:
-        By_v_y = fig.add_subplot(horiz, vert, i); i += 1
+        By_v_y = fig.add_subplot(vert, horiz, i, xlabel = "y", ylabel = "By"); i += 1
     if By_show == True and z_show == True:
-        By_v_z = fig.add_subplot(horiz, vert, i); i += 1
+        By_v_z = fig.add_subplot(vert, horiz, i, xlabel = "z", ylabel = "By"); i += 1
     if Bz_show == True and x_show == True:
-        Bz_v_x = fig.add_subplot(horiz, vert, i); i += 1
+        Bz_v_x = fig.add_subplot(vert, horiz, i, xlabel = "x", ylabel = "Bz"); i += 1
     if Bz_show == True and y_show == True:
-        Bz_v_y = fig.add_subplot(horiz, vert, i); i += 1
+        Bz_v_y = fig.add_subplot(vert, horiz, i, xlabel = "y", ylabel = "Bz"); i += 1
     if Bz_show == True and z_show == True:
-        Bz_v_z = fig.add_subplot(horiz, vert, i)
+        Bz_v_z = fig.add_subplot(vert, horiz, i, xlabel = "z", ylabel = "Bz")
 
     for i in range(first_arg, len(sys.argv)):
         (x, y, z, Bx, By, Bz) = parse_file(sys.argv[i])
         if Bx_show == True and x_show == True:
             Bx_v_x.plot(x, Bx)
         if Bx_show == True and y_show == True:
-            Bx_v_y.plot(y, Bx)
+            Bx_v_y.plot(y, Bx) 
         if Bx_show == True and z_show == True:
             Bx_v_z.plot(z, Bx)
         if By_show == True and x_show == True:
