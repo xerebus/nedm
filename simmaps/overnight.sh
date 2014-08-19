a3a212da7631b630b17a0ae03bd3c6ec5d40cc1d
Aritra Biswas <abiswas@caltech.edu>
2014-08-19 15:58:26 -0700

    Prep for overnight axial field studies

---
+++ simmaps/overnight.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd axial1100mmTall/
+./exec.sh
+cd ..
+
+cd axial1105mmTall/
+./exec.sh
+cd ..
+
+cd axial1110mmTall/
+./exec.sh
+cd ..
+
+cd axialSetMetglas5mmShorter/
+./exec.sh
+cd ..
+
+cd axialSetMetglas5mmTaller/
+./exec.sh
+cd ..
