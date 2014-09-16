37029ee32a381ee6160bb94e2c6237b70da5146d
Aritra Biswas <abiswas@caltech.edu>
2014-09-16 10:13:19 -0700

    Started metglas 1mm thickness runs

--- simmaps/edit.sh
+++ simmaps/edit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-RUNLIST=$(ls | grep metglas5mm_ | grep _SG)
+RUNLIST=$(ls | grep metglas1mm_ | grep _SG)
 
 for FOLDER in $RUNLIST; do
     cd $FOLDER
-    sed -i "s/grid 50 50 200/grid 25 25 100/g" exec.sh
+    sed -i "s/t -1.08 0.367 1.08 0.367 0.005 10000 120 0/t -1.08 0.367 1.08 0.367 0.001 10000 120 0/g" exec.sh
     cd ..
 done
---
+++ simmaps/metglas1mm_axial1100mm_SG/exec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ROTSHIELD="$HOME/rotationshield/RotationShield"
+
+$ROTSHIELD \
+    dir . \
+    field \
+        c \
+            geom 15 0.324 2.146 1 \
+            kdist 0.005 \
+        x \
+    x \
+    bound \
+        n 128 \
+        t -1.08 0.367 1.08 0.367 0.001 10000 120 0 \
+        t -1.08 0.394 1.100 0.394 0.005 0 120 0 \
+    x \
+    cell \
+        range 0.0125 -0.025 -0.10 0.0505 0.025 0.10 \
+        grid 25 25 100 \
+        svgrd 1 \
+    x \
+    solve \
+    apply \
+    meas \
+x
