d9295b4b68b0eab06ab96ebad67d4af0fa9bc030
Aritra Biswas <abiswas@caltech.edu>
2014-09-16 14:16:24 -0700

    1mm SG runs + 2cm consistent SG runs

--- simmaps/get_gradients.sh
+++ simmaps/get_gradients.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 GRAD_EXEC="../../gradients"
-RUNLIST=$(ls | grep metglas2cm_ | grep _SG)
+RUNLIST=$(ls | grep _SG)
 
 for FOLDER in $RUNLIST; do
     cd $FOLDER
-    echo "*** Starting gradients job: $FOLDER"
-    $GRAD_EXEC $(pwd) > gradients.out  &
-    echo "*** Backgrounded."
+    if [ ! -f gradients.out ]; then
+        echo "*** Starting gradients job: $FOLDER"
+        $GRAD_EXEC $(pwd) > gradients.out &
+        echo "*** Backgrounded."
+    fi
     cd ..
 done
