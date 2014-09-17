3368bd0b8f9d9e3f72c44fc3eee3648d79a6d72f
Aritra Biswas <abiswas@caltech.edu>
2014-09-16 19:15:28 -0700

    Gradients for everything

--- simmaps/get_gradients.sh
+++ simmaps/get_gradients.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 GRAD_EXEC="../../gradients"
-RUNLIST=$(ls | grep _SG | grep -v metglas5mm_)
+RUNLIST=$(ls | grep _SG)
 
 for FOLDER in $RUNLIST; do
     cd $FOLDER
-    if [ ! -f gradients.out ]; then
-        echo "*** Starting gradients job: $FOLDER"
-        $GRAD_EXEC $(pwd) > gradients.out &
-        echo "*** Backgrounded."
-    fi
+    echo "*** Starting gradients job: $FOLDER"
+    $GRAD_EXEC $(pwd) > gradients.out &
+    echo "*** Backgrounded."
     cd ..
 done
