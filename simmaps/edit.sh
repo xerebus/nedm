1f2eed5e6dad894f4328b3012fef262d32db3fe7
Aritra Biswas <abiswas@caltech.edu>
2014-09-24 14:55:24 -0700

    NG runs with new gridding - high x, low y

--- simmaps/edit.sh
+++ simmaps/edit.sh
@@ -4,6 +4,6 @@ RUNLIST=$(ls | grep _NG)
 
 for FOLDER in $RUNLIST; do
     cd $FOLDER
-    sed -i "s/grid 25 25 100/grid 25 25 75/g" exec.sh
+    sed -i "s/grid 25 25 75/grid 50 5 75/g" exec.sh
     cd ..
 done
