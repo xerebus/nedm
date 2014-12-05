b858de3f05d7f920fe1f76e1b6f6f5c71e4fdd25
Aritra Biswas <abiswas@caltech.edu>
2014-12-04 17:05:40 -0800

    TS sim ranges fixed

--- simmaps/edit.sh
+++ simmaps/edit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-RUNLIST=$(ls | grep _NG)
+RUNLIST=$(ls | grep _TS)
 
 for FOLDER in $RUNLIST; do
     cd $FOLDER
-    sed -i "s/grid 25 25 75/grid 50 5 75/g" exec.sh
+    sed -i "s/range 0.0125 -0.025 -0.10 0.0505 0.025 0.10/range -0.12 -0.12 0.0 0.12 0.12 0.6/g" exec.sh
     cd ..
 done
