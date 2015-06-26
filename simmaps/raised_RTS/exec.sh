d6d79b96c3043ad9252e1176dd653bbe11e7f443
Aritra Biswas <abiswas@caltech.edu>
2015-06-25 17:06:26 -0700

    RTS - measured third-scale dimensions

---
+++ simmaps/raised_RTS/exec.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ROTSHIELD="$HOME/rotationshield/RotationShield"
+
+$ROTSHIELD \
+    dir . \
+    field \
+        c \
+            geom 15 0.324 0.752 1 \
+            kdist 0.005 \
+        x \
+    x \
+    bound \
+        n 128 \
+        t -1.919 0.360 0.593 0.360 0.005 10000 120 0 \
+    x \
+    cell \
+        range -0.060 -0.060 -0.200 0.060 0.060 0.200 \
+        grid 24 24 80 \
+        svgrd 1 \
+    x \
+    solve \
+    apply \
+    meas \
+x
